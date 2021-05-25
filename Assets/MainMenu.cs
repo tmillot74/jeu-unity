600fba6eec2836168753a873e15a3f03f9da09ce
Churlet Sylvain <56892538+Eksay@users.noreply.github.com>
2021-05-25 16:25:22 +0200

    Update menu

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -1,7 +1,6 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
@@ -11,4 +10,3 @@ public class MainMenu : MonoBehaviour
         Application.Quit();
     }
 }
-
